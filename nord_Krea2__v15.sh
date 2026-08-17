d0f1e76713784df69bcd6f9c1766b0a0192dc982
Norby123 <norbertgardonyi@gmail.com>
2026-08-17 15:22:55 +0200

    Update nord_Krea2__v15.sh

--- nord_Krea2__v15.sh
+++ nord_Krea2__v15.sh
@@ -69,6 +69,8 @@ cd /workspace/ComfyUI/custom_nodes
 
 CUSTOM_NODES=(
     "https://github.com/spacepxl/ComfyUI-VAE-Utils"
+    "https://github.com/harukimix/KreaSeedVarianceEnhancer"
+    "https://github.com/Blakeem/ComfyUI-ContextAnchoredTileRefine"
     "https://github.com/ltdrdata/ComfyUI-Impact-Subpack"
     "https://github.com/cubiq/ComfyUI_essentials"
     "https://github.com/numz/ComfyUI-SeedVR2_VideoUpscaler"
